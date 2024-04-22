c8c5edc56d3000a7b60824cebb5957a0e980bb91
Gaks3 <dwi66116@gmail.com>
2024-04-22 20:42:31 +0700

    feat: lucia config

---
+++ src/server/auth.ts
@@ -0,0 +1,37 @@
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { Lucia } from "lucia";
+import { db } from "./db";
+
+const adapter = new PrismaAdapter(db.session, db.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    name: "movie_token",
+    attributes: {
+      secure: false,
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      id: attributes.id,
+      username: attributes.username,
+      email: attributes.email,
+      isAdmin: attributes.isAdmin,
+    };
+  },
+});
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  id: string;
+  username: string;
+  email: string;
+  isAdmin: boolean;
+}
