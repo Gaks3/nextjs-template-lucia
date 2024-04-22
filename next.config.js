ab9dc9b4bbad2b42156c6a99b4e901a91b7824a6
Gaks3 <dwi66116@gmail.com>
2024-04-22 20:44:56 +0700

    feat: nextjs config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  webpack: (config) => {
+    config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
+    return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default config;
